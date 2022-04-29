8ff44cd79434eccf62f8beb8282e2e4e5c8a28a1
MateCon <mateoziffer@gmail.com>
2022-04-29 09:40:09 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1
+        className="text-3xl font-bold text-blue-500 text-center"
+      >Hello World!</h1>
+    </div>
+  )
+}
+
+export default Home
