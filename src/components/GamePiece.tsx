0bc46946666a0264ca5b891484d799c2bff3466a
MateCon <mateoziffer@gmail.com>
2022-05-06 12:01:12 -0300

    kind of moving pieces

--- src/components/GamePiece.tsx
+++ src/components/GamePiece.tsx
@@ -1,5 +1,7 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Image from "next/image";
+import { useSpring, animated } from '@react-spring/web'
+import { useDrag } from '@use-gesture/react'
 
 interface Props {
     piece: string,
@@ -10,11 +12,31 @@ interface Props {
 const SQUARE_SIZE = 48;
 
 const GamePiece: FC<Props> = ({ piece, x, y }) => {
-    return <div
+    const [xPosition, setXPosition] = useState(x * SQUARE_SIZE);
+    const [yPosition, setYPosition] = useState(y * SQUARE_SIZE);
+
+    const bind = useDrag(({ down, movement: [mx, my] }) => {
+        if (down) {
+            setXPosition(x * SQUARE_SIZE + mx);
+            setYPosition(y * SQUARE_SIZE + my);
+        } else {
+            console.log(
+                mx + SQUARE_SIZE / 2,
+                -my + SQUARE_SIZE / 2
+            )
+            console.log(
+                Math.floor((mx - SQUARE_SIZE / 2) / SQUARE_SIZE),
+                Math.floor((my - SQUARE_SIZE / 2) / SQUARE_SIZE)
+            )
+        }
+    })
+
+    return <animated.div
+        {...bind()}
         className={`absolute w-12 h-12 rounded-full overflow-hidden`}
         style={{
-            top: y * SQUARE_SIZE,
-            left: x * SQUARE_SIZE,
+            top: yPosition,
+            left: xPosition,
         }}
     >
         <Image
@@ -22,8 +44,9 @@ const GamePiece: FC<Props> = ({ piece, x, y }) => {
             alt="piece"
             layout="fill"
             objectFit="cover"
+            draggable={false}
         />
-    </div>
+    </animated.div>
 }
 
 export default GamePiece;
