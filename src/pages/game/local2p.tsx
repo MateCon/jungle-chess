fe565f449e3922e4680c05a14441663d5a4825db
MateCon <68882595+MateCon@users.noreply.github.com>
2022-05-08 13:07:18 -0300

    add title

--- src/pages/game/local2p.tsx
+++ src/pages/game/local2p.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Head from 'next/head';
 import React, { FC } from 'react';
 import Game from '../../components/Game';
 import Navbar from '../../components/Navbar'
@@ -7,9 +8,21 @@ import gameObjects from '../../constants/grids/default';
 
 const CELL_SIZE = 64;
 
+// createMoveHandler example
+// const createMoveHandler = (move: (move: string, position: [number, number]) => void) => {
+//   return (
+//     <button onClick={() => { move('LR', [0, 0]) }}>
+//       Hi
+//     </button>
+//   )
+// }
+
 const Local2P: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Jungle Chess</title>
+      </Head>
       <Navbar />
       <div className='w-screen h-fit min-h-screen bg-background-900 pt-20 md:pt-0 md:pl-20'>
         <div className='grid w-full h-screen place-items-center shadow-xl'>
