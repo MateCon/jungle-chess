9a2aa1c6504230777d82ca102f65a6f9c75b58aa
MateCon <68882595+MateCon@users.noreply.github.com>
2022-05-27 19:47:00 -0300

    .

--- src/pages/aboutjc.tsx
+++ src/pages/aboutjc.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
         <Navbar />
         <div className='w-screen h-fit min-h-screen bg-background-900 pt-20 md:pt-0 md:pl-20'>
           <main className="w-full min-h-screen pt-10 place-content-center flex flex-row">
-             <div className="columns-1 ml-16">
+             <div className="ml-16">
              <h1
                 className="text-4xl font-semibold text-center mb-10"
               >
@@ -17,15 +17,15 @@ const Home: NextPage = () => {
                 <span className="text-primary-600">Jungle</span>
                 <span className="text-white">Chess</span>
               </h1>
-            <div className="flex flex-col justify-center items-center ml-16 box-content bg-background-700 h-[600px] w-[600px]">
-            <h1
+            <div className="flex flex-col justify-center items-center rounded-md shadow-sm mx-8 px-8 box-content bg-background-700 h-[600px] w-[600px]">
+            <h2
               className="text-2xl font-semibold text-center mt-10 mb-2"
               >
                 <span className="text-white mr-2">What's</span>
                 <span className="text-primary-600">Jungle</span>
                 <span className="text-white">Chess?</span>
-            </h1>
-            <h1
+            </h2>
+            <div
               className="text-xl font-normal text-center mb-10"
               >
                 <span className="text-primary-600 font-semibold">Jungle</span>
@@ -40,21 +40,21 @@ const Home: NextPage = () => {
                 <span className="text-primary-600 font-semibold mr-2">Jungle</span>
                 <span className="text-white font-semibold">Chess</span>
                 <span className="text-white mr-2">.</span><br/>
-            </h1>    
-            <h1
+            </div>    
+            <h2
               className="text-2xl font-semibold text-center mt-10 mb-2"
               >
                 <span className="text-white mr-2">How to play</span>
                 <span className="text-primary-600">Jungle</span>
                 <span className="text-white">Chess?</span>
-            </h1>
-            <h1
+            </h2>
+            <div
               className="text-xl font-normal text-center mb-10"
               >           
                 <span className="text-white">There are 8 pieces for each player which have different features.</span><br/>
                 <span className="text-white">The main goal is getting into the opponent's lair, 
                 but you can also eat all the pieces of the another player.</span>
-            </h1>
+            </div>
             </div>
             </div>
           </main>
