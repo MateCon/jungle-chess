c076dcf76f97b527b1e0787c92c449f5d28f4643
TobiasVallejos05 <tochu0505@gmail.com>
2022-05-29 01:27:48 -0300

    about jungle chess page information v2

--- src/pages/aboutjc.tsx
+++ src/pages/aboutjc.tsx
@@ -17,32 +17,34 @@ const Home: NextPage = () => {
                 <span className="text-primary-600">Jungle</span>
                 <span className="text-white">Chess</span>
               </h1>
-            <div className="flex flex-col justify-center items-center rounded-md shadow-sm mx-8 px-8 box-content bg-background-700 h-[1000px] w-[600px]">
+              <div className="flex flex-col">
+              <div className="flex flex-row">
+            <div className="flex flex-col justify-center items-center rounded-md shadow-sm mx-8 px-8 mt-10 mb-10 box-content bg-background-700 h-[1200px] w-[600px]">
             <h2
-              className="text-2xl font-semibold text-center mt-10 mb-2"
+              className="text-2xl font-semibold text-center mt-10 mb-10"
               >
                 <span className="text-white mr-2">What's</span>
                 <span className="text-primary-600">Jungle</span>
                 <span className="text-white">Chess?</span>
             </h2>
             <div
-              className="text-xl font-normal text-center mb-10"
+              className="text-xl font-normal text-left mb-10"
               >
                 <span className="text-primary-600 font-semibold">Jungle</span>
                 <span className="text-white font-semibold mr-2">Chess</span> 
                 <span className="text-white mr-2">is a website based on a board game called</span>
                 <span className="text-white font-semibold mr-2">"Dou Shou Qi"</span>
-                <span className="text-white">which was created a long time ago in China.</span><br/>
+                <span className="text-white mr-2">which was created a long time ago in China.</span>
                 <span className="text-white mr-2">This game is also known as</span>
                 <span className="text-white font-semibold">Animal Chess</span>
                 <span className="text-white mr-2">,</span>
-                <span className="text-white mr-2">but we prefer to call it as another of its name,</span>
+                <span className="text-white mr-2">but we prefer to call it as another of its names,</span>
                 <span className="text-primary-600 font-semibold mr-2">Jungle</span>
                 <span className="text-white font-semibold">Chess</span>
                 <span className="text-white mr-2">.</span><br/>
             </div>    
             </div>
-            <div className="flex flex-col justify-center items-center rounded-md shadow-sm mx-8 px-8 mt-10 box-content bg-background-700 h-[1000px] w-[600px]">
+            <div className="flex flex-col justify-center items-center rounded-md shadow-sm mx-8 px-8 mt-10 mb-10 box-content bg-background-700 h-[1200px] w-[600px]">
             
             <h2
               className="text-2xl font-semibold text-center mt-10 mb-2"
@@ -51,74 +53,161 @@ const Home: NextPage = () => {
                 <span className="text-primary-600 mr-2">Jungle</span>
                 <span className="text-white">Chess?</span>
             </h2>
+            <h2
+              className="text-2xl font-semibold text-center mt-10 mb-10"
+              >
+                <span className="text-white">Squares</span>
+            </h2>
             <div
               className="text-xl font-normal text-center mb-10"
               >           
       
                 
                 <span className="text-white">There are four types of squares.</span><br/>
+
+            <div
+              className="text-xl font-normal text-left mb-10"
+              >   
+            
+            <h2
+              className="mt-4 mb-4">
+                
                 <span className="text-white font-semibold">Lair</span>
                 <span className="text-white mr-2">:</span>
                 <span className="text-white">This is the most important square because it means if the game is over. There is one for each player and their location is in 
                 the middle of the first row on each side. Once a piece has entered the opponent's lair the game is over and the winner is the invader of that lair.</span><br/>
 
+            </h2>
+            <h2
+              className="mt-4 mb-4">
+
                 <span className="text-white font-semibold">Trap</span>
                 <span className="text-white mr-2">:</span>
-                <span className="text-white">This kind of square is located on the sides and next to the lair. Its function is to weaken all the pieces which get into there 
+                <span className="text-white">There are three for each player and they are located on the sides of the lair. Its function is to weaken all the pieces which get into there 
                 so any piece can eat it.</span><br/>
+            </h2>
 
+            <h2
+              className="mt-4 mb-4">
                 <span className="text-white font-semibold">Water</span>
                 <span className="text-white mr-2">:</span>
                 <span className="text-white">There are two lakes in the middle of the board which serve, for some pieces as a shortcut and for others impossible to pass.</span><br/>
-
-
+            </h2>
+            <h2
+              className="mt-4 mb-4">
                 <span className="text-white font-semibold">Land</span>
                 <span className="text-white mr-2">:</span>
                 <span className="text-white">This is the most common square of the game. It is the starting position of all the pieces.</span><br/>
+            </h2>
+            </div>
 
-                <span className="text-white">There are 8 pieces for each player which have different features.</span>
-                <span className="text-white">They are going to be listed from least valuable to most ones</span><br/>
 
+
+
+            <h2
+              className="text-2xl font-semibold text-center mt-10 mb-10"
+              >
+                <span className="text-white">Pieces</span>
+            </h2>
+                <span className="text-white mr-2">There are eight pieces for each player which have different features. They are going to be listed 
+                from least valuable to most ones. </span><br/>
+
+
+            <div
+              className="text-xl font-normal text-left mb-10"
+              > 
+            <h2
+              className="mt-4 mb-4">
                 <span className="text-white font-semibold">Rat</span>
                 <span className="text-white mr-2">:</span>
                 <span className="text-white">This is the weakest piece of the game. Although all others can eat it, the rat is the only one which can eat the strongest piece which is the elephant. 
                 None has this particular feature. The rat can move into the water as if it were land</span><br/>
-
+            </h2>
+            <h2
+              className="mt-4 mb-4">
+                
                 <span className="text-white font-semibold">Cat</span>
                 <span className="text-white mr-2">:</span>
                 <span className="text-white"></span><br/>
+            </h2>
+            <h2
+              className="mt-4 mb-4">
 
                 <span className="text-white font-semibold">Wolf</span>
                 <span className="text-white mr-2">:</span>
                 <span className="text-white"></span><br/>
-
+            </h2>
+            <h2
+              className="mt-4 mb-4">
                 <span className="text-white font-semibold">Dog</span>
                 <span className="text-white mr-2">:</span>
                 <span className="text-white"></span><br/>
-
+            </h2>
+            <h2
+              className="mt-4 mb-4">
                 <span className="text-white font-semibold">Cheetah</span>
                 <span className="text-white mr-2">:</span>
                 <span className="text-white"></span><br/>
+            </h2>
+            <h2
+              className="mt-4 mb-4">
 
                 <span className="text-white font-semibold">Lion</span>
                 <span className="text-white mr-2">:</span>
                 <span className="text-white"></span><br/>
-
+            </h2>
+            <h2
+              className="mt-4 mb-4">
                 <span className="text-white font-semibold">Tiger</span>
                 <span className="text-white mr-2">:</span>
                 <span className="text-white"></span><br/>
-                
+            </h2>
+            <h2
+              className="mt-4 mb-4">
                 <span className="text-white font-semibold">Elephant</span>
                 <span className="text-white mr-2">:</span>
                 <span className="text-white">As mentioned earlier, the elephant is the strongest piece of the game. It can eat all the pieces. However, the rat, which is
                 the weakest one, can eat it. Neither the elephant moves into water nor jumps over it.</span><br/>
-                
-                
+            </h2>      
+            </div>        
         
-
+            <h2
+              className="text-2xl font-semibold text-center mt-10 mb-10"
+              >
+                <span className="text-white">Objective</span>
+            </h2>
 
                 <span className="text-white">The main goal is getting into the opponent's lair, 
                 but you can also eat all the pieces of the another player.</span>
+            </div>
+            </div>
+            </div>
+            <div className="flex flex-col">
+            <div className="flex flex-row">
+            <div className="flex flex-col justify-center items-center rounded-md shadow-sm mx-8 px-8 mt-10 mb-10 box-content bg-background-700 h-[1200px] w-[600px]">
+            <h2
+              className="text-2xl font-semibold text-center mt-10 mb-10"
+              >
+                <span className="text-white mr-2">Contact</span>
+                <span className="text-white">us!</span>
+            </h2>
+             
+            </div>
+            
+            
+            <div className="flex flex-col justify-center items-center rounded-md shadow-sm mx-8 px-8 mt-10 mb-10 box-content bg-background-700 h-[1200px] w-[600px]">
+            <h2
+              className="text-2xl font-semibold text-center mt-10 mb-10"
+              >
+                <span className="text-white mr-2">What's</span>
+                <span className="text-primary-600">Jungle</span>
+                <span className="text-white">Chess?</span>
+            </h2>
+             
+            </div>
+
+
+            </div>
             </div>
             </div>
             </div>
