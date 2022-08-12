2de3ae861d1daa7ea47b72690cb15a51b7d382b3
John Doe <johndoe@example.com>
2022-08-12 09:14:40 -0300

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,24 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { UserProvider } from '@auth0/nextjs-auth0'
+import { io, Socket } from "socket.io-client"
+import { createContext } from 'react'
+import { DefaultEventsMap } from '@socket.io/component-emitter'
+
+export const socketContext = createContext<Socket<DefaultEventsMap, DefaultEventsMap>>(io("http://localhost:8081"));
+const socket = io("http://localhost:8081");
 
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </UserProvider>
+    <socketContext.Provider value={socket}>
+      <UserProvider>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </UserProvider>
+    </socketContext.Provider>
   )
 }
 
